e49356b91d9efc0a4b5fe8ecac0268a191195795
Natasa Mori <natasa@the-mori.com>
2017-12-07 09:23:22 +0100

    Initial commit. Setup of fragments.

---
+++ app/src/main/java/com/example/android/tourguideapp/CategoryAdapter.java
@@ -0,0 +1,52 @@
+package com.example.android.tourguideapp;
+
+/**
+ * Created by natasam on 05/12/2017.
+ */
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Provides the appropriate Fragment for a view pager.
+ */
+
+public class CategoryAdapter extends FragmentPagerAdapter {
+    private Context mContext;
+
+
+    public CategoryAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new ToSeeFragment();
+        } else if (position == 1){
+            return new ForKidsFragment();
+        } else {
+            return new ToEatFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return mContext.getString(R.string.category_tosee);
+        } else if (position == 1) {
+            return mContext.getString(R.string.category_forkids);
+        } else {
+            return mContext.getString(R.string.category_toeat);
+        }
+    }
+}
